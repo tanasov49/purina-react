bd75e45eb0f619d2e8deae7e4aecbdd9ac0c42ed
tanasov49 <tanasov49@gmail.com>
2022-07-27 13:36:54 +0200

    updates

--- src/components/DbcCodes.js
+++ src/components/DbcCodes.js
@@ -3,17 +3,19 @@ export default function DbcCodes(props) {
     return (
         <ul className="dbc-codes">
             {attributes.map((items, index) => {
-  return (
-    <li className="dbc-list" key={index}>
-    {Object.keys(items).map((key) => {
-      return (
-        <p className="dbc-list__text" key={key + index}>{key}:{items[key]}</p>
-      )
-    })}
-    </li>
-  )
-})}
+                return (
+                    <li className="dbc-list" key={index}>
+                        {Object.keys(items).map(key => {
+                            if (items[key] === "0") {
+                                return (
+                                    <p className="dbc-list__text" key={key + index}>{key}</p>
+                                        )
+                        }
+                        return null;
+                        })}
+                    </li>
+                        )
+            })}
         </ul>
     )
-
 }
